c19990a23df5c29623cfe9fd9a45069a8611bb59
Pandyaka Aptanagi <pandyaka.aptanagi@gmail.com>
2020-08-15 14:13:53 +0700

    change eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
         sourceType: 'module', // Allows for the use of imports
-        project: 'tsconfig.json'
+        project: ['tsconfig.json', '.eslintrc.js']
     },
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
