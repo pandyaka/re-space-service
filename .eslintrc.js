c620371068586667148ab81c65d2e3d9bd272e7e
Pandyaka Aptanagi <pandyaka.aptanagi@gmail.com>
2020-08-15 16:58:12 +0700

    change eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,22 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parser: '@typescript-eslint/parser',
     extends: [
         'airbnb-typescript/base',
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
         'plugin:jest/recommended',
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
         'plugin:import/errors',
         'plugin:import/warnings',
-        'plugin:import/typescript'    
+        'plugin:import/typescript',
+        'plugin:prettier/recommended'
     ],
     plugins: ['filenames'],
     parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
+        ecmaVersion: 2018,
+        sourceType: 'module',
         project: ['tsconfig.json', '.eslintrc.js']
     },
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         'import/no-default-export': 'error',
         'import/no-named-default': 'off',
         'import/prefer-default-export': 'off',
